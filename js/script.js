d7e17fb6044f687b705dd40397a2e464cb706d38
kakon4366 <kakonbarman4366@gmail.com>
2022-02-14 23:39:39 +0600

    Top Players and Top Blogs Color added

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//task-1: top players and top blogs color change
+const topPlayer = document.getElementById('top-player');
+const topBlog = document.getElementById('top-blog');
+
+topPlayer.style.color = 'rgba(163, 5, 165, 0.8)';
+topBlog.style.color = 'rgba(163, 5, 165, 0.8)';
+
